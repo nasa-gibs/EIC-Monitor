9039d7449dd2139a0ddb4c0762f310aabe1bfa25
PatchesMaps <patrick.moulden@gmail.com>
2024-10-07 13:22:29 -0400

    Update satellite scenarios and URLs

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 export default {
     1: ['MODIS_Terra_CorrectedReflectance_TrueColor'],
     2: ['GOES-East_ABI_GeoColor', 'GOES-West_ABI_GeoColor', 'Himawari_AHI_Band3_Red_Visible_1km'],
-    3: ['VIIRS_SNPP_Thermal_Anomalies_375m_Day', 'VIIRS_SNPP_CorrectedReflectance_TrueColor'],
+    3: ['VIIRS_SNPP_CorrectedReflectance_TrueColor'],
     4: ['VIIRS_SNPP_DayNightBand_At_Sensor_Radiance'],
     5: ['IMERG_Precipitation_Rate'],
     6: ['GHRSST_L4_MUR_Sea_Surface_Temperature'],
     7: ['MODIS_Aqua_Land_Surface_Temp_Day', 'MODIS_Aqua_CorrectedReflectance_TrueColor'],
     8: ['AMSRU2_Sea_Ice_Concentration_12km'],
     9: ['AMSRU2_Sea_Ice_Concentration_12km'],
-    10: ['VIIRS_NOAA20_Thermal_Anomalies_375m_All', 'VIIRS_NOAA20_CorrectedReflectance_TrueColor'],
+    10: ['VIIRS_NOAA20_CorrectedReflectance_TrueColor'],
     11: ['OMI_Nitrogen_Dioxide_Tropo_Column', 'VIIRS_NOAA20_CorrectedReflectance_TrueColor'],
     12: ['AIRS_L3_Carbon_Monoxide_500hPa_Volume_Mixing_Ratio_Daily_Night', 'AIRS_L3_Carbon_Monoxide_500hPa_Volume_Mixing_Ratio_Daily_Day'],
     13: ['VIIRS_SNPP_AOT_Dark_Target_Land_Ocean', 'VIIRS_SNPP_AOT_Deep_Blue_Best_Estimate', 'VIIRS_SNPP_CorrectedReflectance_TrueColor'],
@@ -17,7 +17,7 @@ export default {
     16: ['VIIRS_NOAA20_DayNightBand_At_Sensor_Radiance'],
     17: ['VIIRS_NOAA20_DayNightBand_AtSensor_M15'],
     18: ['VIIRS_NOAA20_AOT_Dark_Target_Land_Ocean', 'VIIRS_NOAA20_AOT_Deep_Blue_Best_Estimate', 'VIIRS_NOAA20_CorrectedReflectance_TrueColor'],
-    19: ['VIIRS_NOAA21_Thermal_Anomalies_375m_All', 'VIIRS_NOAA21_CorrectedReflectance_TrueColor']
+    19: ['VIIRS_NOAA21_CorrectedReflectance_TrueColor']
   };
   
   export const scenarioUrls = {
